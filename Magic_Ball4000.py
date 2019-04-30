155081c674ea3f68ab3007d8b417074e765e929a
Stephen Glombicki <42524856+theprokills@users.noreply.github.com>
2019-04-30 18:19:18 -0500

    Add files via upload

---
+++ Magic_Ball4000.py
@@ -0,0 +1,167 @@
+# Import the modules
+import sys
+import random
+
+ans = True
+
+while ans:
+    question = input(
+        "Ask the all knowing Magic 100 ball: (Code on Github) (Made in Python 3.7.3) (press enter to quit) ")
+
+    answers = random.randint(1, 50)
+
+    if question == "":
+        sys.exit()
+
+    elif answers == 1:
+        print("It is certain")
+
+    elif answers == 2:
+        print("Outlook good")
+
+    elif answers == 3:
+        print("You may rely on it")
+
+    elif answers == 4:
+        print("Ask again later")
+
+    elif answers == 5:
+        print("Concentrate and ask again")
+
+    elif answers == 6:
+        print("Reply hazy, try again")
+
+    elif answers == 7:
+        print("My reply is no")
+
+    elif answers == 8:
+        print("My sources say no")
+
+    elif answers == 9:
+        print("Why do you need to know.")
+
+    elif answers == 10:
+        print("Stephen is all knowing so go ask him")
+
+    elif answers == 11:
+        print("Just forget about it.")
+
+    elif answers == 12:
+        print("Ask your mom see knows all.")
+
+    elif answers == 13:
+        print("You always knew it.")
+
+    elif answers == 14:
+        print("ALL MY SOURCES SAY NO.")
+
+    elif answers == 14:
+        print("Better not tell you now.")
+
+    elif answers == 15:
+        print("Outlook not so good.")
+
+    elif answers == 16:
+        print("Count on it.")
+
+    elif answers == 17:
+        print("Just Forget about it.")
+
+    elif answers == 18:
+        print("IDK. Try again.")
+
+    elif answers == 19:
+        print("Ask google.")
+
+    elif answers == 20:
+        print("I am not your assistent.")
+
+    elif answers == 21:
+        print("Ask your neighbor, not me.")
+
+    elif answers == 22:
+        print("Noway!")
+
+    elif answers == 23:
+        print("Do a backflip and ask again.")
+
+    elif answers == 24:
+        print("Put food in power port and ask again.")
+
+    elif answers == 25:
+        print("Somewhat.")
+
+    elif answers == 26:
+        print("My all mighty anwser is no.")
+
+    elif answers == 27:
+        print("Probaly No")
+
+    elif answers == 28:
+        print("Probaly Yes")
+
+    elif answers == 29:
+        print("YeSsSsSsSs")
+
+    elif answers == 30:
+        print("Sure")
+
+    elif answers == 31:
+        print("Stop spamming the machine.")
+
+    elif answers == 32:
+        print("Hmmmmmmmmmmmmmmmm. NO")
+
+    elif answers == 33:
+        print("Maybe Yes.")
+
+    elif answers == 34:
+        print("Sadly No.")
+
+    elif answers == 35:
+        print("Sadly Yes.")
+
+    elif answers == 36:
+        print("This is a waste of your time.")
+
+    elif answers == 37:
+        print("Thats a stupid question.")
+
+    elif answers == 38:
+        print("404 Error")
+
+    elif answers == 39:
+        print("What would Stephen do.")
+
+    elif answers == 40:
+        print("Absoulutly.")
+
+    elif answers == 41:
+        print("Outlook not so good")
+
+    elif answers == 42:
+        print("YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSs.")
+
+    elif answers == 43:
+        print("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO.")
+
+    elif answers == 44:
+        print("HMMMMMMMMMMMMMMMMMMMMMMMMMMMM Yes.")
+
+    elif answers == 45:
+        print("Lost in translation")
+
+    elif answers == 46:
+        print("Go ask another machine.")
+
+    elif answers == 47:
+        print("You'll never know.")
+
+    elif answers == 48:
+        print("I am sorry to say but yes.")
+
+    elif answers == 49:
+        print("I am sorry to say but no.")
+
+    elif answers == 50:
+        print("You are stupid to ask that question")
